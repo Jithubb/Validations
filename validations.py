04a95c32482b43ee4d1da644e1c266d3fe6a38be
Jithubb <154482243+Jithubb@users.noreply.github.com>
2023-12-21 09:39:04 +0800

    Added validations.py

---
+++ validations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def validate_username(username, minlen):
+    """Checks if the recieved username matches the required conditions."""
+    if type(username) != str:
+        raise TypeError("Username must be a string")
+    if minlen < 5:
+        raise ValueError("minlen must be at least 5")
+    if len(username < minlen):
+        return False
+    if not username.isalnum():
+        return False
+    if username[0].isnumeric():
+        return False
+    return True 
